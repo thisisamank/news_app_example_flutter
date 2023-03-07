97c6d79c068cf734f9dc7432c94455ad67c0c7da
Aman Kumar <thisisamank@gmail.com>
2023-03-07 23:00:14 +0530

    feat: colors added, removed boilerplate code

---
+++ lib/constants/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const background = Color(0xFF111111);
+  static const yellow = Color(0xFFFFF3C5);
+  static const pink = Color(0xFFFBE5E1);
+  static const blue = Color(0xFFE1F1FF);
+  static const purple = Color(0xFFEAE4FD);
+  static const secondaryBackground = Color(0xFF232323);
+  static const text = Color(0xFF9E9E9E);
+}
--- lib/repository/news_repository.dart
+++ lib/repository/news_repository.dart
@@ -8,13 +8,16 @@ import 'package:news_app_example/secrets.dart';
 class NewsRepository {
   Future<List<Article>?> getArticle() async {
     final response = await http.get(
-        Uri(scheme: 'https', host: NewsApiEndpoints.baseUrl, pathSegments: [
-      'v2',
-      NewsApiEndpoints.everything
-    ], queryParameters: {
-      'apiKey': apiKey,
-      'q': 'bitcoin',
-    }));
+      Uri(
+        scheme: 'https',
+        host: NewsApiEndpoints.baseUrl,
+        pathSegments: ['v2', NewsApiEndpoints.everything],
+        queryParameters: {
+          'apiKey': apiKey,
+          'q': 'bitcoin',
+        },
+      ),
+    );
     if (response.statusCode == 200) {
       final articles = ArticleResponse.fromJson(response.body);
       log(articles.articles.toString());
