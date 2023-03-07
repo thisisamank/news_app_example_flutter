742bb265417b6196a5dbf57d357b5da1a2d76e19
Aman Kumar <thisisamank@gmail.com>
2023-03-07 20:41:11 +0530

    feat: endpoints added

---
+++ lib/constants/endpoints.dart
@@ -0,0 +1,7 @@
+class NewsApiEndpoints {
+  NewsApiEndpoints._();
+
+  static const String baseUrl = 'https://newsapi.org/v2';
+  static const String everything = 'everything';
+  static const String headlines = 'top-headlines';
+}
